5a64af56140d3f0a427e0954c06d7abb474558eb
dsy <dsy@MacBook-Pro-M1.local>
2024-11-25 21:40:53 +0800

    add

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module du-go
 go 1.23.0
 
 require (
+	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/jedib0t/go-pretty/v6 v6.6.2 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
--- main.go
+++ main.go
@@ -2,74 +2,59 @@ package main
 
 import (
 	"fmt"
+	"github.com/dustin/go-humanize"
 	"io/fs"
 	"os"
 	"path/filepath"
+	"slices"
 	"sync"
+	"time"
 )
 
-func calcAllFileSize(root string) (string, int64) {
+func CalcAllFileSizeSum(root string) (string, int64) {
 	var fileSize int64 = 0
-	var mutex sync.Mutex
 	err := filepath.WalkDir(root, func(path string, dir fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
 		if !dir.IsDir() {
 			info, _ := dir.Info()
-			mutex.Lock()
+
 			fileSize += info.Size()
-			mutex.Unlock()
 		}
 		return nil
 	})
 	if err != nil {
-		panic(err)
+		fmt.Errorf("%s error: %v", root, err)
 	}
 	return root, fileSize
 }
 
-func FormatSize(bytes int64) string {
-	const (
-		B  = 1
-		KB = 1024 * B
-		MB = 1024 * KB
-		GB = 1024 * MB
-		TB = 1024 * GB
-	)
-
-	switch {
-	case bytes >= TB:
-		return fmt.Sprintf("%.2f TB", float64(bytes)/float64(TB))
-	case bytes >= GB:
-		return fmt.Sprintf("%.2f GB", float64(bytes)/float64(GB))
-	case bytes >= MB:
-		return fmt.Sprintf("%.2f MB", float64(bytes)/float64(MB))
-	case bytes >= KB:
-		return fmt.Sprintf("%.2f KB", float64(bytes)/float64(KB))
-	default:
-		return fmt.Sprintf("%d B", bytes)
-	}
-}
-
 func main() {
-
-	//basePath := os.Args[1]
-	basePath := "/Users/dsy/workplaces/"
+	st := time.Now()
+	basePath := os.Args[1]
 	entries, _ := os.ReadDir(basePath)
+	exclude := []string{"share"}
 	var wg sync.WaitGroup
 	for _, entry := range entries {
+		if slices.Contains(exclude, entry.Name()) {
+			fmt.Println("Skipping", entry.Name())
+			continue
+		}
+
 		wg.Add(1)
 		go func(e os.DirEntry) {
 			defer wg.Done()
+			fullpath := filepath.Join(basePath, e.Name())
 			if e.IsDir() {
-				root, fileSize := calcAllFileSize(filepath.Join(basePath, e.Name()))
-				fmt.Printf("%s,%s\n", root, FormatSize(fileSize))
+				_, fileSize := CalcAllFileSizeSum(fullpath)
+				fmt.Printf("%s,%s\n", fullpath, humanize.Bytes(uint64(fileSize)))
 			} else {
 				info, _ := e.Info()
-				fmt.Printf("%s,%s\n", filepath.Join(basePath, e.Name()), FormatSize(info.Size()))
+				fmt.Printf("%s,%s\n", fullpath, humanize.Bytes(uint64(info.Size())))
 			}
 		}(entry)
 	}
 	wg.Wait()
+	fmt.Println("Total time:", time.Since(st))
 }
